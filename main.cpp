1d5b1360c7db019a125e617367ceb89916d74eca
Skygg3 <skygge00@gmail.com>
2019-12-18 20:05:13 +0100

    Problem 2

--- main.cpp
+++ main.cpp
@@ -12,9 +12,29 @@ int problem1()
     return sum;
 }
 
+long long problem2()
+{
+    int fibn2 = 1;
+    int fibn1 = 2;
+    int fibn = fibn1 + fibn2;
+    long long sum = 2;
+
+    while(fibn < 4000000)
+    {
+        if(fibn%2 == 0) sum += fibn;
+
+        fibn2 = fibn1;
+        fibn1 = fibn;
+        fibn = fibn1 + fibn2;
+    }
+
+    return sum;
+}
+
 int main()
 {
     std::cout << problem1() << '\n';
+    std::cout << problem2() << '\n';
 
     return 0;
 }
